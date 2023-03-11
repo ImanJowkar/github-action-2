fa5e58d9a429816cb4a3642ff28e63798f873ccd
Iman <imanjowkar99@gmail.com>
2023-03-11 18:43:12 +0000

    add a comment in branch dev-example

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-
+//This is a new comment 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
